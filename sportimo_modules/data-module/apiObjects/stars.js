106efee0e649a778c22daea3882cb493a8a8dafe
aris@bedbugstudio.com <aris@bedbugstudio.com>
2018-11-30 14:33:02 +0200

    Modified stars model, and stars get endpoint logic in data-module/apiObjects

--- sportimo_modules/data-module/apiObjects/stars.js
+++ sportimo_modules/data-module/apiObjects/stars.js
@@ -4,51 +4,22 @@
 
 // ALL
 api.getAll = function (cb) {
-    var q = Stars.findOne({});
-    q.populate('users.user', { username: 1, level: 1, picture: 1 });
+    //var q = Stars.findOne({});
+    //q.populate('users.user', { username: 1, level: 1, picture: 1 });
 
-    return q.exec(function (err, starsDoc) {
-        if (!starsDoc)
-            cbf(cb, err, []);
+    //return q.exec(function (err, starsDoc) {
+    //    if (!starsDoc)
+    //        cbf(cb, err, []);
 
-        cbf(cb, err, starsDoc.users);
-    });
+    //    cbf(cb, err, starsDoc.users);
+    //});
+
+    Stars.find({ user: { $ne: null } })
+        .populate({ path: 'user', select: 'username level picture' })
+        .sort({ rank: 1 })
+        .exec(cb);
 };
 
-/*
-// CREATE
-api.add = function (item, cb) {
-
-    if (!item) {
-        cb('Invalid star user data.');
-    }
-
-    star = new Stars(item);
-
-    star.save((err, saved) => {
-        cbf(cb, err, saved.toObject());
-    });
-};
-
-// UPDATE
-api.update = function (id, updateData, cb) {
-    Stars.findByIdAndUpdate(id, updateData, function (err, update) {
-      cbf(cb, err, update.toObject());
-  });// eo achievement.find
-};
-
-// DELETE
-api.remove = function (id, cb) {
-    return Stars.findById(id, function (err, item) {
-        if (item)
-            return item.remove(function (err) {
-            cbf(cb, err, true);
-        });
-    else
-      cbf(cb, err, true);
-  });
-};
-*/
 
 // Helper callback method
 var cbf = function (cb, err, data) {
--- sportimo_modules/models/star.js
+++ sportimo_modules/models/star.js
@@ -9,8 +9,10 @@ if (mongoose.models.stars)
 else {
 
     const titleSchema = new mongoose.Schema({
+        pool: { type: String, ref: 'pool' },
         iconUrl: { type: String },
-        date: { type: String },
+        date: { type: String }, // string representation of endDate formatted as dd/MM/YYYY
+        endDate: { type: Date },
         text: { type: Schema.Types.Mixed }
     });
 
@@ -18,12 +20,14 @@ else {
     const userSchema = new mongoose.Schema({
         rank: { type: Number },
         user: { type: String, ref: 'users', required: true },
+        starsCount: { type: Number, default: 0 },
+        lastStarDate: { type: Date },
         titles: [titleSchema]
     });
 
-    const starSchema = new Schema({
-        users: [userSchema]
-    });
+    //const starSchema = new Schema({
+    //    users: [userSchema]
+    //});
 
-    module.exports = mongoose.model('stars', starSchema);
+    module.exports = mongoose.model('stars', userSchema);
 }
