86dc1e313e438fe2c540369c543c6d2392ac2d10
aris@bedbugstudio.com <aris@bedbugstudio.com>
2019-12-18 20:27:24 +0200

    Changed mongodb production user identity

--- config/mongoConfig.js
+++ config/mongoConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
     production: {
         url: 'ds021165-a0.mlab.com:21165,ds021165-a1.mlab.com:21165/sportimo2?replicaSet=rs-ds021165',
-        user: 'bedbug',
-        password: "a21th21"
+        user: 'gameserver',
+        password: "g%40m3s3rv3r"
     },
     development: {
        url: 'ds127550-a0.mlab.com:27550,ds127550-a1.mlab.com:27550/sportimo_development?replicaSet=rs-ds127550',
